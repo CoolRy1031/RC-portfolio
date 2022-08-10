4347e0a49e148ce20b0dda27d71c5081f7afd979
CoolRy <that007guy@hotmail.com>
2022-08-10 10:15:08 -0400

    got most of my info where I want

---
+++ js/app.js
@@ -0,0 +1,13 @@
+import { projectData } from "./project-data.js";
+
+const cardContainer = document.getElementById('card-container')
+
+let projectMarkup = projectData.map(project =>
+  `<nav id="nav card-container">
+  <a class="nav-link active">${project.title}</a>
+  <div class="icons">
+  <a class="fa fa-github-square"style="font-size:18px; color: silver" href="${project.github}"></a>
+  <a class="fa fa-rocket"style="font-size:18px; color: silver" href="${project.deployment}"></a></div>
+</nav>`
+).join('')
+cardContainer.innerHTML = projectMarkup
---
+++ js/project-data.js
@@ -0,0 +1,26 @@
+let projectData = [
+  {
+    title: 'Tic-Tac Toe',
+    deployment: 'https://ttt-ryanc.netlify.app/',
+    github: 'https://github.com/CoolRy1031/ttt-weekend',
+  },
+  {
+    title: 'Disc-Golf App',
+    deployment: 'https://coolry1031discgolf.herokuapp.com/',
+    github: 'https://github.com/CoolRy1031/Disc-golf-app',
+  },
+  {
+    title: 'Stranger Drinks',
+    deployment: 'https://stranger-drinks.netlify.app/',
+    github: 'https://github.com/CoolRy1031/stranger-drinks-front-end',
+  },
+  {
+    title: 'Code-Cards',
+    deployment: 'https://sei-code-cards.netlify.app/',
+    github: 'https://github.com/CoolRy1031/code-cards-front-end',
+  },
+]
+
+export {
+  projectData
+}
